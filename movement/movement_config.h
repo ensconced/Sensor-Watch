6f3cff1980b5a9b80d00f9a403db57c23b5e09dd
Joe Barnett <joe.barnett@elliptic.co>
2023-10-29 20:34:24 +0000

    change faces

--- movement/movement_config.h
+++ movement/movement_config.h
@@ -31,12 +31,16 @@ const watch_face_t watch_faces[] = {
     simple_clock_face,
     totp_face,
     sunrise_sunset_face,
-    moon_phase_face,
     stopwatch_face,
-    preferences_face,
-    set_time_face,
+    timer_face,
+    wake_face,
+
+    moon_phase_face,
     thermistor_readout_face,
-    voltage_face
+    voltage_face,
+    chirpy_demo_face,
+    preferences_face,
+    set_time_face
 };
 
 #define MOVEMENT_NUM_FACES (sizeof(watch_faces) / sizeof(watch_face_t))
@@ -47,7 +51,7 @@ const watch_face_t watch_faces[] = {
  * Some folks also like to use this to hide the preferences and time set faces from the normal rotation.
  * If you don't want any faces to be excluded, set this to 0 and a long Mode press will have no effect.
  */
-#define MOVEMENT_SECONDARY_FACE_INDEX (MOVEMENT_NUM_FACES - 2) // or (0)
+#define MOVEMENT_SECONDARY_FACE_INDEX (MOVEMENT_NUM_FACES - 6) // or (0)
 
 /* Custom hourly chime tune. Check movement_custom_signal_tunes.h for options */
 #define SIGNAL_TUNE_DEFAULT
